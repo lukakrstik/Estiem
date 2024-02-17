beb3e850e7675217dd68da75b5a8ed1ce1136f41
Luka Krstikj <luka.krstik@protonmail.com>
2024-02-17 16:05:13 +0100

    Tailwind added, Sign In page styled.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        krstic: '#171717',
+        siva: '#eeeeee',
+        estiem: '#205e44'
+      }
+    },
+  },
+  plugins: [],
+}
+
